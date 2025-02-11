106887e97923d14088ef25f165e1fb05ed8bd58c
Yashraj8959 <ykushwah7828@gmail.com>
2025-02-11 17:10:01 +0530

    Add profile route and authentication middleware to user routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { registerController, loginUserController } = require('../controllers/user.controller')
+const { registerController, loginUserController, profileUserController } = require('../controllers/user.controller')
+const { authUser } = require('../middlewares/user.middleware')
 const router = express.Router()
 
 router.post('/register',registerController)
 router.post('/login',loginUserController)
+router.get('/profile', authUser ,profileUserController)
 
 module.exports = router
