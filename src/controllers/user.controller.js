34382a428d6bc3d4620bbd161718108c31152e28
Yashraj8959 <ykushwah7828@gmail.com>
2025-02-11 13:55:06 +0530

    Refactor user registration and login controllers to improve error handling and validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,38 +4,49 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
 module.exports.registerController = async (req,res)=>{
-    const {username, email, password} = req.body;
+    try {
+        
+        const {name, email, password} = req.body;
+        
+        if(!name){
+            return res.status(400).json({message: "user name is required"})
+        }
+        if(!email){
+            return res.status(400).json({message: "email is required"})
+        }
+        if(!password){
+            return res.status(400).json({message: "password is required"})
+        }
     
-    if(!username){
-        return res.status(400).json({message: "username is required"})
+        const isUserExist = await UserModel.findOne({
+            $or:[
+                {email},
+                {name}
+            ]
+        })
+    
+        if(isUserExist){
+            return res.status(400).json({message: "User already exists"})
+        }
+    
+        const hashedPassword = await bcrypt.hash(password, 10)
+    
+        const newuser = await UserModel.create({
+            name,
+            email,
+            password:hashedPassword
+        })
+    
+        const token = jwt.sign({_id: newuser._id,email: newuser.email},config.JWT_SECRET_KEY)
+        res.status(200).json({
+            message: 'user registered successfully',
+            token: token
+        })
+    } catch (error) {
+        console.log("error during register",error)
+        res.status(500).send('Internal server error')
     }
-    if(!email){
-        return res.status(400).json({message: "email is required"})
-    }
-    if(!password){
-        return res.status(400).json({message: "password is required"})
-    }
-
-    const isUserExist = UserModel.findOne({
-        $or:[
-            {email},
-            {username}
-        ]
-    })
-
-    if(isUserExist){
-        return res.status(400).json({message: "User already exists"})
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-    const user = await UserModel.create({
-        username,
-        email,
-        password:hashedPassword
-    })
-
-    const token = jwt.sign({email,username},config.JWT_SECRET_KEY)
+    
 
 
 
@@ -44,18 +55,35 @@ module.exports.registerController = async (req,res)=>{
 
 
 module.exports.loginUserController = async (req,res) => {
-    const {email,password} = req.body;
-    if(!email){
-        return res.status(400).json({message: "email is required"})
+    try{
+        const {email,password} = req.body;
+        if(!email){
+            return res.status(400).json({message: "email is required"})
+        }
+
+        if(!password){
+            return res.status(400).json({message: "password is required"})
+        }
+
+        const isUserExist = await UserModel.findOne({email})
+
+        if(!isUserExist){
+            return res.status(400).json({message: "Invalid email or password"})
+        }
+
+        const user = await UserModel.findOne({email})
+        const isMatch = await bcrypt.compare(password,user.password )
+
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid email or password"})
+        }
+
+        const token = jwt.sign({email:user.email,_id:user._id},config.JWT_SECRET_KEY)
+        console.log('Token: ', token)
+        res.status(200).json({message: 'logged in',token: token})
     }
-
-    if(!password){
-        return res.status(400).json({message: "password is required"})
-    }
-
-    const isUserExist = await UserModel.findOne({email})
-
-    if(!isUserExist){
-        
+    catch(error){
+        console.error('Error during login', error)
+        res.status.send('Internal Server Error')
     }
 }
