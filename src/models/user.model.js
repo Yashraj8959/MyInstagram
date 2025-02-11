89ca08017a1282dff79625199f6f535cd40f98b9
Yashraj8959 <ykushwah7828@gmail.com>
2025-02-11 16:50:13 +0530

    Add JWT and bcrypt methods for token generation and password hashing in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -46,6 +49,27 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+
+
+
 const UserModel = mongoose.model('User', userSchema);
+
+userSchema.methods.generateToken = function() {
+    return jwt.sign({ _id: this._id }, config.JWT_SECRET_KEY);
+}
+
+userSchema.statics.verifyToken = function(token) {
+    return jwt.verify(token, config.JWT_SECRET_KEY);
+}
+
+userSchema.statics.hashpassword = async function(password){
+    return await bcrypt.hash(password, 10);
+}
+
+userSchema.statics.comparePassword = async function(password, hash){
+    return await bcrypt.compare(password, hash);
+}
+
+
 
 module.exports = UserModel;
