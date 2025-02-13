d924774fe043c793f50a3d933cbc3e3ad8122df0
Yashraj8959 <ykushwah7828@gmail.com>
2025-02-14 01:25:27 +0530

    Add CORS and Morgan middleware; implement feed route and controller for fetching posts

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
-const app = express()
 const userRoutes = require('./routes/users.routes')
 const postRoutes = require('./routes/posts.routes')
+const indexRoutes = require('./routes/index.routes')
+const cors = require('cors')
+const morgan = require('morgan')
 
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use('/', indexRoutes)
 app.use('/users',userRoutes)
 app.use('/posts', postRoutes)
 
