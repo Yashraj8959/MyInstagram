ab5f90094bca1e5f8e0be74686fce613cad57bda
Yashraj8959 <ykushwah7828@gmail.com>
2025-02-28 00:33:43 +0530

    Add cookie-parser and imagekit; update API routes and middleware configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,17 +4,19 @@ const postRoutes = require('./routes/posts.routes')
 const indexRoutes = require('./routes/index.routes')
 const cors = require('cors')
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
-app.use(cors())
+app.use(cookieParser())  // added cookie parser middleware for storing JWT token in cookies.  //
+app.use(cors())   // added cors middleware for enabling cross-origin resource sharing. //
 app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json())  // added json middleware for parsing incoming request bodies. //
+app.use(express.urlencoded({extended: true}))  // added urlencoded middleware for parsing incoming request bodies. //
 
 app.use('/', indexRoutes)
-app.use('/users',userRoutes)
-app.use('/posts', postRoutes)
+app.use('/v1/api/users',userRoutes)
+app.use('/v1/api/posts', postRoutes)
 
 
 module.exports = app;
